cdc8d9327b9982face47d81355656861406f2b69
pauloseifer <paulo.seifer@gmail.com>
2022-11-22 10:53:25 -0300

    Primeiro push

---
+++ hello.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python3
+
+print("Content-type:text/html\r\n\r\n")
+print("<html>")
+print("<head>")
+print("<title>Olá mundo - Python CGI</title>")
+print("</head>")
+print("<body>")
+print("<h2> Olá mundo!!! </h2>")
+print("</body>")
+print("</html>")
